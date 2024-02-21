c64de27433cf1e3efeb7146e91c5413e76f10cf4
bsfonseca <bs.fonseca12@gmail.com>
2024-02-21 10:45:31 -0300

    feat: criação de modal para loading

---
+++ src/config/modules/loading.slice.ts
@@ -0,0 +1,13 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const loadingSlice = createSlice({
+    name: "loading",
+    initialState: false,
+    reducers: {
+        setLoading: (state, action: PayloadAction<boolean>) => {
+            return action.payload;
+        },
+    },
+});
+export default loadingSlice.reducer;
+export const { setLoading } = loadingSlice.actions;
--- src/config/rootReducer.ts
+++ src/config/rootReducer.ts
@@ -1,8 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import alunoSlice from "./modules/aluno.slice";
+import loadingSlice from "./modules/loading.slice";
 
-const combinedReducers = combineReducers({});
+const combinedReducers = combineReducers({
+    aluno: alunoSlice,
+    loading: loadingSlice,
+});
 
 export const persistedReducers = persistReducer(
     {
